14e46449a9d214580e60fe070b344ebe665c4922
王涛 <wangtao@fxiaoke.com>
2016-04-01 19:21:13 +0800

    add sign

---
+++ ui/src/main/java/com/ys/ui/common/sign/Configure.java
@@ -0,0 +1,9 @@
+package com.ys.ui.common.sign;
+
+/**
+ * Created by wangtao on 2016/4/1.
+ */
+public class Configure {
+
+    public static String SIGNKEY = "xoxogzboaizhigu2016xxooxxoo";
+}
---
+++ ui/src/main/java/com/ys/ui/common/sign/Signature.java
@@ -0,0 +1,78 @@
+package com.ys.ui.common.sign;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * User: rizenguo
+ * Date: 2014/10/29
+ * Time: 15:23
+ */
+public class Signature {
+    /**
+     * 签名算法
+     * @param o 要参与签名的数据对象
+     * @return 签名
+     * @throws IllegalAccessException
+     */
+    public static String getSign(Object o) throws IllegalAccessException {
+        ArrayList<String> list = new ArrayList<String>();
+        Class cls = o.getClass();
+        Field[] fields = cls.getDeclaredFields();
+        for (Field f : fields) {
+            f.setAccessible(true);
+            if (f.get(o) != null && f.get(o) != "") {
+                list.add(f.getName() + "=" + f.get(o) + "&");
+            }
+        }
+        int size = list.size();
+        String [] arrayToSort = list.toArray(new String[size]);
+        Arrays.sort(arrayToSort, String.CASE_INSENSITIVE_ORDER);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < size; i ++) {
+            sb.append(arrayToSort[i]);
+        }
+        String result = sb.toString();
+        result += "key=" + Configure.SIGNKEY;
+        Util.log("Sign Before MD5:" + result);
+        result = MD5.MD5Encode(result).toUpperCase();
+        Util.log("Sign Result:" + result);
+        return result;
+    }
+
+    public static String getSign(Map<String,Object> map){
+        ArrayList<String> list = new ArrayList<String>();
+        for(Map.Entry<String,Object> entry:map.entrySet()){
+            if(entry.getValue()!=""){
+                list.add(entry.getKey() + "=" + entry.getValue() + "&");
+            }
+        }
+        int size = list.size();
+        String [] arrayToSort = list.toArray(new String[size]);
+        Arrays.sort(arrayToSort, String.CASE_INSENSITIVE_ORDER);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < size; i ++) {
+            sb.append(arrayToSort[i]);
+        }
+        String result = sb.toString();
+        result += "key=" + Configure.SIGNKEY;
+        //Util.log("Sign Before MD5:" + result);
+        result = MD5.MD5Encode(result).toUpperCase();
+        //Util.log("Sign Result:" + result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("key1", "211");
+        map.put("key3", "211");
+        map.put("key2", "211");
+        map.put("key4", "211");
+        System.out.printf(getSign(map));
+
+    }
+
+}
