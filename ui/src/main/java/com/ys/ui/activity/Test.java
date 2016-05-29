d5e67d257aaac031d647ab12ef1eb0c504ef217e
wang-tao927@163.com <meilin@1216>
2016-05-29 20:05:36 +0800

    update footer

---
+++ ui/src/main/java/com/ys/ui/activity/Test.java
@@ -0,0 +1,7 @@
+package com.ys.ui.activity;
+
+/**
+ * Created by river on 2016/5/29.
+ */
+public class Test {
+}
---
+++ ui/src/main/java/com/ys/ui/activity/TestActivity.java
@@ -0,0 +1,16 @@
+package com.ys.ui.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.ys.ui.R;
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+
+}
